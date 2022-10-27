54f1e7bcc1a9797d3ff4e519c82c6b5f1b68f96d
Mario Navarro <menavarroc@protonmail.com>
2022-10-27 00:59:58 -0500

    main fields of Address, Municipality and States entities were added

--- src/main/java/com/mgdental/app/domain/address/entity/Address.java
+++ src/main/java/com/mgdental/app/domain/address/entity/Address.java
@@ -1,2 +1,30 @@
-package com.mgdental.app.domain.address.entity;public class Address {
+package com.mgdental.app.domain.address.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column
+    private String address;
+    @Column(name = "postal_code")
+    private Long postalCode;
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_municipal")
+    private Municipality idMunicipal;
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "id_state")
+    private State idState;
 }
--- src/main/java/com/mgdental/app/domain/address/entity/State.java
+++ src/main/java/com/mgdental/app/domain/address/entity/State.java
@@ -1,2 +1,12 @@
-package com.mgdental.app.domain.address.entity;public class State {
+package com.mgdental.app.domain.address.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "state_name")
+    private String stateName;
 }
