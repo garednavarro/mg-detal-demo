8d71d43eeb53a555dfeada7aa6498bd724607fb1
Mario Navarro <menavarroc@protonmail.com>
2022-10-26 21:12:05 -0500

    Speciality class created

---
+++ src/main/java/com/mgdental/app/domain/user/entity/Speciality.java
@@ -0,0 +1,23 @@
+package com.mgdental.app.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="specility")
+public class Speciality {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "speciality_name")
+    private String name;
+}
